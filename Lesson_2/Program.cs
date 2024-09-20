3570edb8de0544f955ba8d30d9088a6788430b3e
Timothy <Thron41k@gmail.com>
2024-09-20 13:35:37 +0800

    Добавлен вывод в консоль символов "@" и "#"

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,7 +1,8 @@
-﻿
-const string myName = "Tim";
+﻿const string myName = "Tim";
 Console.WriteLine(myName);
 Console.WriteLine("Привет, мир");
 Console.WriteLine("Мне 34 года");
 Console.WriteLine($"My name is {myName}");
+Console.WriteLine("\u0040");
+Console.WriteLine("\u0023");
 Console.ReadKey();
