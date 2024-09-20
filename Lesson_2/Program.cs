1afb61cf524fc4af52fc5b72c6a5dd291981c032
Timothy <Thron41k@gmail.com>
2024-09-20 13:30:29 +0800

    Задание 3.2.5

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,3 +1,7 @@
-﻿const string myName = "Tim";
+﻿
+const string myName = "Tim";
 Console.WriteLine(myName);
+Console.WriteLine("Привет, мир");
+Console.WriteLine("Мне 34 года");
+Console.WriteLine($"My name is {myName}");
 Console.ReadKey();
